032174be23f7e503c24786f8cf6f33d960b5999e
Fullofb3ans <igorgazinskii6@gmail.com>
2025-02-19 11:10:20 +0400

    iss/изменил ссылку к апи

--- front/vite.config.js
+++ front/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     }
   },
   define: {
-    'API_URL': JSON.stringify('http://regconf.emk.org.ru/api')
+    'API_URL': JSON.stringify('http://regconf.emk.ru/api')
   }
 
 })
