a0d6651433519d3ab171b18af7837c96d25fbc07
kyberlox <alexxx.timofeev.00@gmail.com>
2025-02-26 16:15:49 +0400

    Отладка пользовательской активности

--- code/src/User.py
+++ code/src/User.py
@@ -232,7 +232,9 @@ class User:
 
         try:
             self.uuid = decode(self.token, key="emk", algorithms=["HS512"])['uuid']
+            print(self.uuid)
             Redis = UserRedis(uuid=self.uuid, jsn=self.current_json)
+            print(Redis.get_user())
             if Redis.get_user() is not None:
                 return True
             else:
