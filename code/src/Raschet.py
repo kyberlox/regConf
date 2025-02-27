54b5810853cb18a7ef85e2491ffcdf02f4ac1312
kyberlox <alexxx.timofeev.00@gmail.com>
2025-02-27 10:14:03 +0400

    Отладка пользовательской активности

--- code/src/Raschet.py
+++ code/src/Raschet.py
@@ -694,7 +694,7 @@ def get_tightness(dt):
     dt["tightness"] = tightness 
     return dt
 
-def make_XL(dt, ID):
+def make_XL(dt):
     WB = load_workbook("./src/ТКП.xlsx")
     sheet = WB['Лист1']
 
