608fd4a6565fd216cbac1de6560bbb42dcc4d0f4
kyberlox <alexxx.timofeev.00@gmail.com>
2025-02-28 16:40:42 +0400

    Отладка загрузки XL

--- code/main.py
+++ code/main.py
@@ -549,6 +549,7 @@ def generate(data = Body(), name = Header(default=None) , token = Header(default
         # получить название и сохранить в БД
         #получить json для генерации из Redis
         jsn = usr.create_TKP(name)
+        print(jsn)
     else:
         jsn = data
 
