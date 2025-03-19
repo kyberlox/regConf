c75e9f6be304a1f54e475b97825bb36ce6faa947
kyberlox <alexxx.timofeev.00@gmail.com>
2025-03-19 12:28:51 +0400

    Подбор PN по Pn

--- code/src/Raschet.py
+++ code/src/Raschet.py
@@ -68,7 +68,7 @@ db = SessionLocal()
 
 
 def searchT2(T, Pn):
-    print(f"T2: {Pn}")
+    #print(f"T2: {Pn}")
     #найти все подходящие строки их DNS и P1 - больше искомых
     request = db.query(Table2).filter(Table2.T >= T, Table2.Pn >= Pn).all()
 
@@ -89,11 +89,11 @@ def searchT2(T, Pn):
                 "Pn" : example.Pn * 10, 
                 "PN" : example.P
             }
-    #print(ans)
+    print(ans)
     return ans
     
 def searchT10(T, Pn):
-    print(f"T10: {Pn}")
+    #print(f"T10: {Pn}")
     #найти все подходящие строки их DNS и P1 - больше искомых
     request = db.query(Table10).filter(Table10.T >= T, Table10.Pn >= Pn).all()
 
