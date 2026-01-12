b4daa03e8e4f007c11a526720574da559dc36615
Gazinskiy Igor <113348311+Fullofb3ans@users.noreply.github.com>
2026-01-12 12:18:39 +0400

    bug/новая ссылка на интранет для авторизации

--- front/src/assets/staticJsons/errors/autorizeErrors.js
+++ front/src/assets/staticJsons/errors/autorizeErrors.js
@@ -2,5 +2,5 @@ export const autorizeErrors = [{
     id: 1,
     type: 'autorizeError',
     inputName: 'tkpError',
-    text: `<a class='helper__message-link' href='https://portal.emk.ru/intranet/tools/regconf.php'>Авторизуйтесь</a> для сохранения ТКП и получения доступа к истории запросов`
+    text: `<a class='helper__message-link' href='https://intranet.emk.ru/api/auth_router/regconf'>Авторизуйтесь</a> для сохранения ТКП и получения доступа к истории запросов`
 }];
