01cc594e68bd2ac07a4451cdc7ec58809c79cf4d
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-01-13 10:39:32 +0400

    отладка авторизации

--- code/src/User.py
+++ code/src/User.py
@@ -258,6 +258,7 @@ class User:
     def set_dt(self):
         #загрузить json
         user_info = check_session_id(self.token)
+        print(self.token, user_info, 'че приходит')
         if 'authenticated' in user_info.keys() and user_info['authenticated'] is True:
             self.uuid = user_info['user']['uuid']
         else:
